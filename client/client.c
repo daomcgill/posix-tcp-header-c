7e4dfc98f8fa0a362b95c3b1d26d5895749eb63c
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-11-21 21:05:54 -1000

    complete

--- client/client.c
+++ client/client.c
@@ -136,6 +136,156 @@ void setHeader(struct TCP_header *header, uint16_t src_port, uint16_t dest_port)
 }
 
 /**
+ * FUNCTION: headerArray
+ *
+ * DESCRIPTION: convert header struct to array of bytes
+ *
+ * @param header: TCP header struct instance
+ * @param array: array of bytes
+ *
+ * @return: void
+ **/
+void headerArray(struct TCP_header *header, uint8_t *array)
+{
+    *((uint16_t *)array) = htons(header->src_port);
+    array += 2;
+    *((uint16_t *)array) = htons(header->dest_port);
+    array += 2;
+    *((uint32_t *)array) = htonl(header->seq_num);
+    array += 4;
+    *((uint32_t *)array) = htonl(header->ack_num);
+    array += 4;
+    *((uint8_t *)array) = header->data_offset;
+    array += 1;
+    *((uint8_t *)array) = header->flags;
+    array += 1;
+    *((uint16_t *)array) = htons(header->window);
+    array += 2;
+    *((uint16_t *)array) = htons(header->checksum);
+    array += 2;
+    *((uint16_t *)array) = htons(header->urg_ptr);
+}
+
+/**
+ * FUNCTION: printHeader
+ * 
+ * DESCRIPTION: Prints the human readable format of the TCP header.
+ *
+ * @param headerArray: Pointer to the byte array representing the TCP header.
+ * @param size: The size of the TCP header array.
+ * 
+ * @return: void
+ **/
+void printHeader(uint8_t *array, size_t size) 
+{
+    if (size < 20) 
+    {
+        printf("Array size is too small to represent a TCP header.\n");
+        return;
+    }
+
+    printf("  Source Port:      %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Destination Port: %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Sequence Number:  %u\n", ntohl(*((uint32_t *)array)));
+    array += 4;
+    printf("  Acknowledgment:   %u\n", ntohl(*((uint32_t *)array)));
+    array += 4;
+    uint8_t data_offset = *array >> 4;
+    printf("  Data Offset:      %u\n", data_offset);
+    
+    uint16_t flagsField = ntohs(*((uint16_t *)array)) & 0x01FF; 
+    printf("  Flags:\n");
+    printf("    NS:  %u\n", (flagsField >> 8) & 1); 
+    printf("    CWR: %u\n", (flagsField >> 7) & 1); 
+    printf("    ECE: %u\n", (flagsField >> 6) & 1); 
+    printf("    URG: %u\n", (flagsField >> 5) & 1); 
+    printf("    ACK: %u\n", (flagsField >> 4) & 1); // ACK
+    printf("    PSH: %u\n", (flagsField >> 3) & 1);
+    printf("    RST: %u\n", (flagsField >> 2) & 1); 
+    printf("    SYN: %u\n", (flagsField >> 1) & 1); // 
+    printf("    FIN: %u\n", flagsField & 1);       
+    array += 2;
+    printf("  Window:           %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Checksum:         %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Urgent Pointer:   %u\n", ntohs(*((uint16_t *)array)));
+}
+
+/**
+ * FUNCTION: printRawHeader
+ * 
+ * DESCRIPTION: Prints the raw bytes of the TCP header.
+ *
+ * @param headerArray: Pointer to the byte array representing the TCP header.
+ * @param size: The size of the TCP header array.
+ * 
+ * @return: void
+ **/
+void printRawHeader(const uint8_t *headerArray, size_t size) {
+    printf("Raw TCP Header Bytes:\n");
+    for (size_t i = 0; i < size; ++i) {
+        printf("%02X ", headerArray[i]);
+        if ((i + 1) % 8 == 0) { 
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+/**
+ * FUNCTION: setHeaderReceived
+ * 
+ * DESCRIPTION: sets header according to received data
+ * 
+ * @param headerArray: array of data to set header to
+ * @param header: header to set
+ * 
+*/
+void setHeaderReceived(const uint8_t *headerArray, struct TCP_header *header) {
+    header->src_port = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->dest_port = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->seq_num = ntohl(*((uint32_t *)headerArray));
+    headerArray += 4;
+    header->ack_num = ntohl(*((uint32_t *)headerArray));
+    headerArray += 4;
+    header->data_offset = *headerArray >> 4;
+    headerArray += 1;
+    header->flags = *headerArray;
+    headerArray += 1;
+    header->window = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->checksum = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->urg_ptr = ntohs(*((uint16_t *)headerArray));
+}
+
+/**
+ * FUNCTION: setHeaderSend
+ * 
+ * DESCRIPTION: sets header according to received data
+ * 
+ * @param headerArray: array of data to set header to
+ * @param header: header to set
+ * 
+*/
+void setHeaderSend(struct TCP_header *receivedHeader, struct TCP_header *sendHeader) {
+    sendHeader->src_port = receivedHeader->dest_port;
+    sendHeader->dest_port = receivedHeader->src_port;
+    sendHeader->seq_num = receivedHeader->ack_num;
+    sendHeader->ack_num = htonl(ntohl(receivedHeader->seq_num)) + 20;
+    sendHeader->data_offset = receivedHeader->data_offset;
+    sendHeader->window = receivedHeader->window;
+    sendHeader->checksum = receivedHeader->checksum;
+    sendHeader->urg_ptr = receivedHeader->urg_ptr;
+    sendHeader->flags = (receivedHeader->flags | (1 << 1) | (1 << 4));
+}
+
+/**
  * FUNCTION: main
  * 
  * DESCRIPTION: Main function of server.
@@ -163,8 +313,6 @@ int main(int argc, char *argv[])
    int tcp = SOCK_STREAM;
    int sock = 0;
 
-   
-
    sock = createSocket(ipv4, tcp);
    if (sock >= 0 && rv == 0)
    {
@@ -182,6 +330,53 @@ int main(int argc, char *argv[])
             /* create tcp header */
             struct TCP_header header;
             setHeader(&header, clientPort, port);
+            uint8_t headArray[20];
+            headerArray(&header, headArray);
+            
+            if (send(sock, headArray, sizeof(headArray), 0) > 0)
+            {
+                printBoth(file, "TCP HEADER SENT\n");
+                printHeader(headArray, sizeof(headArray));
+                printRawHeader(headArray, sizeof(headArray));
+                uint8_t receivedHeaderArray[20];
+                ssize_t read_bytes = read(sock, receivedHeaderArray, sizeof(receivedHeaderArray));
+                if (read_bytes > 0) {
+                    struct TCP_header receivedHeader;
+                    setHeaderReceived(receivedHeaderArray, &receivedHeader);
+
+                    struct TCP_header responseHeader;
+                    setHeaderSend(&receivedHeader, &responseHeader);
+
+                    uint8_t responseHeaderArray[20];
+                    headerArray(&responseHeader, responseHeaderArray);
+                    send(sock, responseHeaderArray, sizeof(responseHeaderArray), 0);
+                     headerArray(&receivedHeader, headArray);
+                     printBoth(file, "RECEIVED TCP HEADER\n");
+                     printHeader(headArray, sizeof(headArray));
+                     printRawHeader(headArray, sizeof(headArray));
+
+                     struct TCP_header sendHeader;
+                     setHeaderSend(&receivedHeader, &sendHeader); 
+                     uint8_t sendHeaderArray[20];
+                     headerArray(&sendHeader, sendHeaderArray);
+                     printBoth(file, "SENT TCP HEADER\n");
+                     printHeader(sendHeaderArray, sizeof(sendHeaderArray));
+                     printRawHeader(sendHeaderArray, sizeof(sendHeaderArray));
+                     if (send(sock, sendHeaderArray, sizeof(sendHeaderArray), 0) < 0) {
+                        printBoth(file, "ERR: send failed\n");
+                        rv = -1;
+                     } else {
+                        printBoth(file, "header sent to client\n");
+                     }
+                }
+
+                
+            }
+            else
+            {
+                printBoth(file, "ERR: header could not be sent to server\n");
+                rv = -1;
+            }
         }
         else
         {
--- server/server.c
+++ server/server.c
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 FILE *file;
 
@@ -136,6 +137,175 @@ int acceptConnection(int sock)
 }
 
 /**
+* STRUCT: tcp_header
+ *
+* DESCRIPTION: Structure to represent the TCP header.
+**/
+struct TCP_header
+{
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint8_t data_offset;
+    uint8_t flags;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urg_ptr;
+};
+
+/**
+ * FUNCTION: setHeaderReceived
+ * 
+ * DESCRIPTION: sets header according to received data
+ * 
+ * @param headerArray: array of data to set header to
+ * @param header: header to set
+ * 
+*/
+void setHeaderReceived(const uint8_t *headerArray, struct TCP_header *header) {
+    header->dest_port = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->src_port = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->seq_num = ntohl(*((uint32_t *)headerArray));
+    headerArray += 4;
+    header->ack_num = ntohl(*((uint32_t *)headerArray));
+    headerArray += 4;
+    header->data_offset = *headerArray >> 4;
+    headerArray += 1;
+    header->flags = *headerArray;
+    headerArray += 1;
+    header->window = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->checksum = ntohs(*((uint16_t *)headerArray));
+    headerArray += 2;
+    header->urg_ptr = ntohs(*((uint16_t *)headerArray));
+}
+
+/**
+ * FUNCTION: setHeaderSend
+ * 
+ * DESCRIPTION: sets header according to received data
+ * 
+ * @param headerArray: array of data to set header to
+ * @param header: header to set
+ * 
+*/
+void setHeaderSend(struct TCP_header *receivedHeader, struct TCP_header *sendHeader) {
+    sendHeader->src_port = receivedHeader->dest_port;
+    sendHeader->dest_port = receivedHeader->src_port;
+    sendHeader->seq_num = htonl(((uint32_t)rand() << 16) | (uint32_t)rand());
+    sendHeader->ack_num = htonl(ntohl(receivedHeader->seq_num)) + 20;
+    sendHeader->data_offset = receivedHeader->data_offset;
+    sendHeader->window = receivedHeader->window;
+    sendHeader->checksum = receivedHeader->checksum;
+    sendHeader->urg_ptr = receivedHeader->urg_ptr;
+    sendHeader->flags = (receivedHeader->flags | (1 << 1) | (1 << 4));
+}
+
+
+/**
+ * FUNCTION: printHeader
+ * 
+ * DESCRIPTION: Prints the human readable format of the TCP header.
+ *
+ * @param headerArray: Pointer to the byte array representing the TCP header.
+ * @param size: The size of the TCP header array.
+ * 
+ * @return: void
+ **/
+void printHeader(uint8_t *array, size_t size) 
+{
+    if (size < 20) 
+    {
+        printf("Array size is too small to represent a TCP header.\n");
+        return;
+    }
+
+    printf("  Source Port:      %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Destination Port: %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Sequence Number:  %u\n", ntohl(*((uint32_t *)array)));
+    array += 4;
+    printf("  Acknowledgment:   %u\n", ntohl(*((uint32_t *)array)));
+    array += 4;
+    uint8_t data_offset = *array >> 4;
+    printf("  Data Offset:      %u\n", data_offset);
+    
+    uint16_t flagsField = ntohs(*((uint16_t *)array)) & 0x01FF; 
+    printf("  Flags:\n");
+    printf("    NS:  %u\n", (flagsField >> 8) & 1); 
+    printf("    CWR: %u\n", (flagsField >> 7) & 1); 
+    printf("    ECE: %u\n", (flagsField >> 6) & 1); 
+    printf("    URG: %u\n", (flagsField >> 5) & 1); 
+    printf("    ACK: %u\n", (flagsField >> 4) & 1); // ACK
+    printf("    PSH: %u\n", (flagsField >> 3) & 1);
+    printf("    RST: %u\n", (flagsField >> 2) & 1); 
+    printf("    SYN: %u\n", (flagsField >> 1) & 1); // SYN
+    printf("    FIN: %u\n", flagsField & 1);       
+    array += 2;
+    printf("  Window:           %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Checksum:         %u\n", ntohs(*((uint16_t *)array)));
+    array += 2;
+    printf("  Urgent Pointer:   %u\n", ntohs(*((uint16_t *)array)));
+}
+
+/**
+ * FUNCTION: printRawHeader
+ * 
+ * DESCRIPTION: Prints the raw bytes of the TCP header.
+ *
+ * @param headerArray: Pointer to the byte array representing the TCP header.
+ * @param size: The size of the TCP header array.
+ * 
+ * @return: void
+ **/
+void printRawHeader(const uint8_t *headerArray, size_t size) {
+    printf("Raw TCP Header Bytes:\n");
+    for (size_t i = 0; i < size; ++i) {
+        printf("%02X ", headerArray[i]);
+        if ((i + 1) % 8 == 0) { 
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+/**
+ * FUNCTION: headerArray
+ *
+ * DESCRIPTION: convert header struct to array of bytes
+ *
+ * @param header: TCP header struct instance
+ * @param array: array of bytes
+ *
+ * @return: void
+ **/
+void headerArray(struct TCP_header *header, uint8_t *array)
+{
+    *((uint16_t *)array) = htons(header->dest_port);
+    array += 2;
+    *((uint16_t *)array) = htons(header->src_port);
+    array += 2;
+    *((uint32_t *)array) = htonl(header->seq_num);
+    array += 4;
+    *((uint32_t *)array) = htonl(header->ack_num);
+    array += 4;
+    *((uint8_t *)array) = header->data_offset;
+    array += 1;
+    *((uint8_t *)array) = header->flags;
+    array += 1;
+    *((uint16_t *)array) = htons(header->window);
+    array += 2;
+    *((uint16_t *)array) = htons(header->checksum);
+    array += 2;
+    *((uint16_t *)array) = htons(header->urg_ptr);
+}
+
+/**
  * FUNCTION: main
  * 
  * DESCRIPTION: Main function of server.
@@ -152,6 +322,7 @@ int main(int argc, char *argv[])
    int tcp = SOCK_STREAM;
    int allip = INADDR_ANY;
    int sock = 0;
+   srand(time(NULL));
 
    //file to write to
    file = fopen("output_server.txt", "w");
@@ -178,31 +349,58 @@ int main(int argc, char *argv[])
                if (conn >= 0 && rv == 0)
                {
                   printBoth(file, "connection established\n");
-                  // char message[50] = "Hello Dao. The server is working!\n";
-                  // send(conn, message, strlen(message), 0);
-                  // printBoth(file, "message sent\n");
-                  FILE *fp = fopen("file_server.jpeg", "rb");
-                  if (fp != NULL)
-                  {
-                     char buff[1000];
-                     int read = 0;
-                     printBoth(file, "sending file...\n");
-                     while ((read = fread(buff, 1, sizeof(buff), fp)) > 0)
-                     {
-                        send(conn, buff, read, 0);
-                     }
-                     printBoth(file, "file sent\n");
-                     fclose(fp);
-                     close(conn);
-                     printBoth(file, "connection to client closed\n");
-                     rv = 1;
-                  }
-                  else
-                  {
-                     printBoth(file, "ERR: could not open file\n");
+                  uint8_t headArray[20] = {0};
+                  ssize_t read_bytes = read(conn, headArray, sizeof(headArray));
+                  if (read_bytes < 0) {
+                     printBoth(file, "ERR: read failed\n");
                      rv = -1;
-                  }
+                  } else if (read_bytes == 0) {
+                     printBoth(file, "Connection closed by client\n");
+                  } else {
+                     printBoth(file, "header received from client: \n");
+                     struct TCP_header receivedHeader;
+                     setHeaderReceived(headArray, &receivedHeader);
+                     headerArray(&receivedHeader, headArray);
+                     printBoth(file, "RECEIVED TCP HEADER\n");
+                     printHeader(headArray, sizeof(headArray));
+                     printRawHeader(headArray, sizeof(headArray));
+
+                     struct TCP_header sendHeader;
+                     setHeaderSend(&receivedHeader, &sendHeader); 
+                     uint8_t sendHeaderArray[20];
+                     headerArray(&sendHeader, sendHeaderArray);
+                     printBoth(file, "SENT TCP HEADER\n");
+                     printHeader(sendHeaderArray, sizeof(sendHeaderArray));
+                     printRawHeader(sendHeaderArray, sizeof(sendHeaderArray));
+                     if (send(conn, sendHeaderArray, sizeof(sendHeaderArray), 0) < 0) {
+                        printBoth(file, "ERR: send failed\n");
+                        rv = -1;
+                     } else {
+                        printBoth(file, "header sent to client\n");
+                     }
+
+                     read_bytes = read(conn, headArray, sizeof(headArray));
+                     if (read_bytes < 0) {
+                        printBoth(file, "ERR: read failed\n");
+                        rv = -1;
+                     } else if (read_bytes == 0) {
+                        printBoth(file, "Connection closed by client\n");
+                     } else {
+                        printBoth(file, "header received from client: \n");
+                        struct TCP_header receivedHeader;
+                        setHeaderReceived(headArray, &receivedHeader);
+                        headerArray(&receivedHeader, headArray);
+                        printBoth(file, "RECEIVED TCP HEADER\n");
+                        printHeader(headArray, sizeof(headArray));
+                        printRawHeader(headArray, sizeof(headArray));
+                     }
+
+
+
+
                }
+                                 
+               } 
                else
                {
                   printBoth(file, "ERR: connection failed\n");
